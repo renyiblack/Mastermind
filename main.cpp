b247e8cc71714d5e077eb2bfa9aea0778acf8348
Victor Ângelo <victorangelograca@hotmail.com>
2016-05-27 20:30:34 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <windows.h>
 #include <fstream>
+#include <conio.h>
 
 using namespace std;
 
@@ -38,6 +39,9 @@ void Menu(int jogadores,char jogador1[20], char jogador2[20], int dificuldade, i
     switch(escolha)
     {
     case 1:
+        if(jogadores==1)
+            jogador2="computador";
+        cout<<"Nome do jogador 1: "<<jogador1<<"\tNome do jogador 2: "<<jogador2<<endl;
         Jogo();
         break;
     case 2:
@@ -54,7 +58,24 @@ void Menu(int jogadores,char jogador1[20], char jogador2[20], int dificuldade, i
 }
 void Jogo()
 {
-    cout<<"Ainda n esta pronto\n";
+    char tabuleiro[10][8], cores[7] {"123456"};
+    for(int i=0; i<10; i++)
+        for(int j=0; j<8; j++)
+            tabuleiro[i][j]='*';
+    cout<<"As opcoes de cores sao: ";
+    for(int i=0; i<6; i++)
+        cout<<cores[i]<<" ";
+    cout<<endl;
+    for(int i=0; i<10; i++)
+    {
+        for(int j=0; j<8; j++)
+        {
+            cout<<tabuleiro[i][j];
+            if(j==3)
+                cout<<"\t";
+        }
+        cout<<endl;
+    }
     system("pause");
 }
 void Instrucoes()
@@ -87,9 +108,7 @@ void Identificacao(int jogadores,char jogador1[20], char jogador2[20])
     cout<<"Digite o nome do jogador 1: ";
     cin.ignore();
     cin.getline(jogador1,20);
-    if(jogadores==1)
-        jogador2="computador";
-    else
+    if(jogadores==2)
     {
         cout<<"Digite o nome do jogador2: ";
         cin.getline(jogador2,20);
